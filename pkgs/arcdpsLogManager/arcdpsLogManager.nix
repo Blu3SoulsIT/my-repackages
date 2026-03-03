e33faccfd2e323a2a4b35e0dea197ccb5df238c9
Blu3Souls <Blu3SoulsIT@gmail.com>
2026-03-03 08:18:51 +0100

    added deprecation notice

--- pkgs/arcdpsLogManager/arcdpsLogManager.nix
+++ pkgs/arcdpsLogManager/arcdpsLogManager.nix
@@ -17,45 +17,48 @@ let
   version = "1.15";
   hash = "sha256-z7SuE+MPhN4/XW3CtYabbAd2ZjL2M/ii+VCdyUUukoA=";
 in
-buildDotnetModule rec {
-  pname = name;
-  inherit version;
-  src = fetchFromGitHub {
-    owner = "gw2scratch";
-    repo = "evtc";
-    tag = "manager-v${version}";
-    inherit hash;
-  };
+buildDotnetModule (
+  finalAttrs:
+  lib.warn "arcdpsLogManager is deprecated. Use arcdps-log-manager from nixpkgs-unstable instead." {
+    pname = name;
+    inherit version;
+    src = fetchFromGitHub {
+      owner = "gw2scratch";
+      repo = "evtc";
+      tag = "manager-v${version}";
+      inherit hash;
+    };
 
-  nugetDeps = ./deps.json;
+    nugetDeps = ./deps.json;
 
-  projectFile = "ArcdpsLogManager.Gtk/ArcdpsLogManager.Gtk.csproj";
+    projectFile = "ArcdpsLogManager.Gtk/ArcdpsLogManager.Gtk.csproj";
 
-  dotnet-sdk = dotnetCorePackages.sdk_8_0;
-  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+    dotnet-sdk = dotnetCorePackages.sdk_8_0;
+    dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  nativeBuildInputs = [
-    wrapGAppsHook3
-  ];
+    nativeBuildInputs = [
+      wrapGAppsHook3
+    ];
 
-  buildInputs = [
-    openssl
-    krb5
-    icu
-  ];
+    buildInputs = [
+      openssl
+      krb5
+      icu
+    ];
 
-  runtimeDeps = [
-    gtk3
-    libnotify
-  ];
+    runtimeDeps = [
+      gtk3
+      libnotify
+    ];
 
-  meta = with lib; {
-    description = "A manager for Guild Wars 2 log files.";
-    longDescription = ''
-      A manager for all your recorded logs. Filter logs, upload them with one click, find interesting statistics. 
-    '';
-    homepage = "https://gw2scratch.com/tools/manager";
+    meta = with lib; {
+      description = "A manager for Guild Wars 2 log files.";
+      longDescription = ''
+        A manager for all your recorded logs. Filter logs, upload them with one click, find interesting statistics. 
+      '';
+      homepage = "https://gw2scratch.com/tools/manager";
 
-    mainProgram = "GW2Scratch.ArcdpsLogManager.Gtk";
-  };
-}
+      mainProgram = "GW2Scratch.ArcdpsLogManager.Gtk";
+    };
+  }
+)
