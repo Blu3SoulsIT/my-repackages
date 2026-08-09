8711d286350491890417cec29a6d2b829be2d18d
Blu3Souls <Blu3SoulsIT@gmail.com>
2026-08-09 13:40:02 +0200

    ported to fhs env

--- pkgs/nuclear/nuclear.nix
+++ pkgs/nuclear/nuclear.nix
@@ -3,13 +3,7 @@
   stdenv,
   fetchurl,
   dpkg,
-  autoPatchelfHook,
-  wrapGAppsHook3,
-  webkitgtk_4_1,
-  gtk3,
-  glib,
-  glib-networking,
-  gst_all_1,
+  buildFHSEnv,
   writeShellApplication,
   nix-update,
 }:
@@ -18,43 +12,116 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "nuclear";
   version = "1.46.1";
 
+  # nix-update can now easily find and update this!
   src = fetchurl {
-    url = "https://github.com/nukeop/nuclear/releases/download/player@${finalAttrs.version}/Nuclear_${finalAttrs.version}_amd64.deb";
+    url = "https://github.com/nukeop/nuclear/releases/download/player@${finalAttrs.version}/nuclear_${finalAttrs.version}_amd64.deb";
     hash = "sha256-loTqy8QKXDr2UdUuolpehLzz0MWs4SqkMKlk6zX5URo=";
   };
 
-  nativeBuildInputs = [
-    dpkg
-    autoPatchelfHook
-    wrapGAppsHook3
-  ];
+  dontUnpack = true;
+  dontBuild = true;
 
-  buildInputs = [
-    webkitgtk_4_1
-    gtk3
-    glib
-    glib-networking
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-  ];
+  # We define the inner components inside the installPhase
+  installPhase =
+    let
 
-  unpackPhase = ''
-    dpkg -x $src .
-  '';
+      # 1. Extract the .deb natively
+      nuclear-extracted = stdenv.mkDerivation {
+        name = "${finalAttrs.pname}-extracted-${finalAttrs.version}";
+        inherit (finalAttrs) src;
+        nativeBuildInputs = [ dpkg ];
+        unpackPhase = "dpkg -x $src .";
+        installPhase = ''
+          mkdir -p $out
+          cp -r usr/* $out/
+          mv $out/bin/nuclear-music-player $out/bin/${finalAttrs.pname}
+        '';
+      };
 
-  installPhase = ''
-    runHook preInstall
+      # 2. Build the pristine FHS sandbox
+      fhs = buildFHSEnv {
+        name = finalAttrs.pname;
+        targetPkgs =
+          pkgs: with pkgs; [
+            nuclear-extracted
 
-    mkdir -p $out
-    cp -r usr/bin $out/
-    cp -r usr/share $out/
+            gtk3
+            webkitgtk_4_1
+            glib
+            glib-networking
+            libsoup_3
+            gdk-pixbuf
+            cairo
+            pango
+            atk
 
-    chmod +x $out/bin/nuclear-music-player
+            at-spi2-core
+            at-spi2-atk
+            dbus
 
-    runHook postInstall
-  '';
+            cacert
+            openssl
+            zlib
+
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            (gst_all_1.gst-plugins-good.override {
+              qt5Support = false;
+              qt6Support = false;
+            })
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-libav
+            alsa-lib
+            libpulseaudio
+
+            mesa
+            libglvnd
+            libGL
+            vulkan-loader
+
+            nss
+            nspr
+            systemd
+            libX11
+            libXcomposite
+            libXdamage
+            libXext
+            libXfixes
+            libXrandr
+            libxcb
+          ];
+
+        profile = ''
+          export WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1
+
+          export GST_PLUGIN_SYSTEM_PATH_1_0=/usr/lib/gstreamer-1.0:/lib/gstreamer-1.0
+          export GST_PLUGIN_PATH_1_0=/usr/lib/gstreamer-1.0:/lib/gstreamer-1.0
+
+          export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+          export SSL_CERT_DIR=/etc/ssl/certs
+
+          export GIO_EXTRA_MODULES=/usr/lib/gio/modules
+          export GIO_MODULE_DIR=/usr/lib/gio/modules
+        '';
+
+        runScript = "nuclear";
+      };
+
+    in
+    ''
+      mkdir -p $out/bin $out/share/applications
+      mkdir -p $out/bin $out/share/icons
+
+      ln -s ${fhs}/bin/${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+      cp ${nuclear-extracted}/share/applications/*.desktop $out/share/applications/${finalAttrs.pname}.desktop
+      cp -r ${nuclear-extracted}/share/icons $out/share/
+
+      substituteInPlace $out/share/applications/${finalAttrs.pname}.desktop \
+      --replace-warn 'Exec=nuclear-music-player' 'Exec=${finalAttrs.pname}' \
+      --replace-warn 'Exec=nuclear' 'Exec=${finalAttrs.pname}'
+    '';
 
   passthru.updateScript = writeShellApplication {
     name = "update-${finalAttrs.pname}";
@@ -69,6 +136,6 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://nuclearplayer.com/";
     license = licenses.agpl3Plus;
     platforms = [ "x86_64-linux" ];
-    mainProgram = "nuclear-music-player";
+    mainProgram = finalAttrs.pname;
   };
 })
